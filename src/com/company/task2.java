9ce2fa90ab9063877ea3921143a50f82aaafdac6
Pavlo_Denysyuk <Pavlogh#1>
2020-07-16 02:02:45 +0300

    add task2

---
+++ src/com/company/task2.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class task2 {
+
+  public static void main(String[] args) {
+    // Вивести на екран всі числа от 1 до 100, які діляться на 5 без залишку.
+
+    for (int i = 1; i <= 100; i++) {
+      if ((i % 5) == 0) {
+        System.out.print(i + " ");
+      }
+    }
+  }
+}
