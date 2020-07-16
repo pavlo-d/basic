aaa1b8f2c7ff4161263d2572be2b0fcdcb592360
Pavlo_Denysyuk <Pavlogh#1>
2020-07-16 22:39:50 +0300

    fix issue ine Task6

--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -4,7 +4,7 @@ public class Task6 {
 
   public static void main(String[] args) {
     //  Знайти в масиві число яке повторюється найчастіше.
-    int[] arr = {2, 2, 3, 4, 1, 2, 1, 1, 1, 5, 5, 1, 1, 3, 1};
+    int[] arr = {2, 2, 3, 4, 1, 2, 1, 1, 1, 5, 5, 1, 1, 3, 1, 1};
     int[] arrDuplicates = new int[arr.length];
     for (int i = 0; i < arr.length; i++) {
       for (int j = 0; j < arr.length; j++) {
@@ -13,12 +13,17 @@ public class Task6 {
         }
       }
     }
+    System.out.print("A = ");
+    for (int item : arr) {
+      System.out.print(item + " ");
+    }
     int max = 0;
     for (int i = 0; i < arrDuplicates.length; i++) {
       if (arrDuplicates[i] > max) {
         max = arrDuplicates[i];
       }
     }
-    System.out.println("Number " + max + " frequency = " + arrDuplicates[max]);
+    System.out.println();
+    System.out.println("Number " + arr[max] + " frequency = " + max);
   }
 }
